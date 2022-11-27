7dc025b0dd3f5673fb3e80aaf0cd8dce35f56cac
bugrahanyucel <bugrahan57@hotmail.com>
2022-11-28 01:22:46 +0300

    Implemented hash function, finished the getMotherboardSN function, initialized the functions that need to be implemented.

--- Client.java
+++ Client.java
@@ -1,8 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.*;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.security.*;
+
+// TODO: Görev dağılımını todo'lar ile yaptım, oradan bakarak yapabilirsin.
+
+/*
+
+    Run the program as "java Client.java from console if you are using Linux
+
+*/
 
 public class Client {
     public Client() {
@@ -13,78 +19,134 @@ public class Client {
         Client client = new Client();
 
         // retrieve network interface's MAC address
-        String mac = client.getMAC();
-        System.out.println("mac = " + mac);
+//        String mac = client.getMAC();
+//        System.out.println("mac = " + mac);
 
-        // Retrieve disk serial number
-
+         // Retrieve disk serial number
+//        String DSN = client.getDiskSN();
+//        System.out.println("DSN = " + DSN);
 
         // Retrieve motherboard's serial number
-        String MBSN = client.getMotherboardSN();
+//        String MBSN = client.getMotherboardSN();
+//        System.out.println("MBSN = " + MBSN);
 
     }
 
     public String getMAC() {
-        StringBuilder builder = new StringBuilder();
-
+        String macHex = "";
         try {
             InetAddress ip = Inet4Address.getLocalHost(); // Get device ip
             NetworkInterface device = NetworkInterface.getByInetAddress(ip); // Get network interface name
 
             byte[] mac = device.getHardwareAddress(); // Get mac address as a byte array
-            builder = new StringBuilder();
 
-            for (int i = 0; i < mac.length; i++) { // Parse bytes to hex string
-                String hex = Integer.toHexString(mac[i]);
-                hex = hex.substring(hex.length() - 2); // Get last two characters (for 2's complement bytes)
-                builder.append(hex);
+            macHex = bytesToHex(mac, ":");
 
-                if (i < mac.length - 1) {
-                    builder.append(":");
-                }
-            }
         } catch (UnknownHostException | SocketException e) {
             e.printStackTrace();
             System.out.println("An error occurred on network configuration");
         }
 
-        return builder.toString();
+        return macHex;
     }
 
-    public String getDiskSN() { // TODO: Barış'ın işi
+    public String getDiskSN() { // TODO: Barış's part
+        // Get the disk serial number of the device
         return null;
     }
 
-    public String getMotherboardSN() { // TODO: Fix this
+    public String getMotherboardSN() { // Note: This function asks for sudo password
         String OS = System.getProperty("os.name").toLowerCase(); // OS name
         String sn = null; // Serial number to return
 
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Console console = System.console();
+
         try {
             // Decide the command based on the operating system
-            String[] cmd = {}; // Calling exec with a string parameter is deprecated
+            String cmd = ""; // Calling exec with a string parameter is deprecated
             if (OS.contains("linux")) { // Linux
-                cmd = new String[]{"sudo dmidecode -s baseboard-serial-number"};
+                cmd = "sudo dmidecode -s baseboard-serial-number";
+//                cmd = "pwd";
             } else if (OS.contains("win")) { // Windows
-                cmd = new String[]{"wmic baseboard get serialnumber"};
+                cmd = "wmic baseboard get serialnumber";
             } else {
                 System.out.println("The program is incompatible with your operating system");
             }
 
-            System.out.println("cmd = " + Arrays.toString(cmd));
-            Process serialNumberProcess = Runtime.getRuntime().exec(cmd);
-            InputStreamReader ISR = new InputStreamReader(serialNumberProcess.getInputStream());
-            BufferedReader br = new BufferedReader(ISR);
-            sn = br.readLine(); // todo trim
+            System.out.println("cmd = " + cmd);
 
+            if (console == null) {
+                System.out.println("No console");
+                return null;
+            }
+
+            // Execute the command
+            Process serialNumberProcess = Runtime.getRuntime().exec(cmd);
             serialNumberProcess.waitFor();
 
-            System.out.println("sn = " + sn);
+            InputStreamReader ISR = new InputStreamReader(serialNumberProcess.getInputStream());
+            BufferedReader br = new BufferedReader(ISR);
+            sn = br.readLine();
 
-        } catch (Exception e) {
+            serialNumberProcess.waitFor();
+            br.close();
 
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
 
-        return null;
-
+        return sn;
     }
+
+    public String bytesToHex(byte[] input, String dlm) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length; i++) { // Parse bytes to hex string
+            String hex = Integer.toHexString(input[i]);
+            hex = hex.substring(hex.length() - 2); // Get last two characters (for 2's complement bytes)
+            sb.append(hex);
+
+            if (i < input.length - 1) {
+                sb.append(dlm);
+            }
+        }
+        return sb.toString();
+    }
+
+    // Returns all the info with "$" as separator
+    public String getAllInfo() {
+        return null;
+    }
+
+    public String getUsernameSerial() { // TODO: Barış's part
+        // Return the username and the serial number (from the file or by use input)
+        return null;
+    }
+
+    public boolean isLicenseExistent() { // TODO: Barış's part
+        // Check if a file named "license.txt" does exist on current directory
+        return false;
+    }
+
+    public byte[] encryptRSA() { // TODO Buğrahan's part
+        StringBuilder sb = new StringBuilder();
+
+        return null;
+    }
+
+    public String MD5(String input) {
+        String hash = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(input.getBytes());
+            byte[] digest = md.digest();
+            hash = bytesToHex(digest, "");
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hash;
+    }
+
+
 }
--- LicenseManager.java
+++ LicenseManager.java
@@ -1,3 +1,44 @@
-public class LicenseManager {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+public class LicenseManager {
+    public LicenseManager() {
+
+    }
+
+    public byte[] decryptRSA() {
+        return null;
+    }
+
+    public String MD5(String input) {
+        String hash = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(input.getBytes());
+            byte[] digest = md.digest();
+            hash = bytesToHex(digest, "");
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hash;
+    }
+
+    public String bytesToHex(byte[] input, String dlm) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length; i++) { // Parse bytes to hex string
+            String hex = Integer.toHexString(input[i]);
+            hex = hex.substring(hex.length() - 2); // Get last two characters (for 2's complement bytes)
+            sb.append(hex);
+
+            if (i < input.length - 1) {
+                sb.append(dlm);
+            }
+        }
+        return sb.toString();
+    }
+
+    public String signHash() {
+        return null;
+    }
 }
